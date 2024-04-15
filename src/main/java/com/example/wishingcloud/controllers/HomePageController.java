0325542418fd6c467eb670507f708f7f4ebc7868
xRipzch <andersravn94@gmail.com>
2024-04-15 15:45:48 +0200

    fuck github

--- src/main/java/com/example/wishingcloud/controllers/HomePageController.java
+++ src/main/java/com/example/wishingcloud/controllers/HomePageController.java
@@ -1,9 +1,11 @@
 package com.example.wishingcloud.controllers;
 import com.example.wishingcloud.services.UserService;
+import com.example.wishingcloud.services.WishlistService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -11,11 +13,22 @@ public class HomePageController {
     @Autowired
     UserService userService;
 
-    @GetMapping ("/homepage")
-    public String homePage(Model model,@RequestParam String email, @RequestParam String password)  {
-        model.addAttribute("user", userService.getUserID(email));
+    @Autowired
+    WishlistService wishService;
+
+    @GetMapping("/homepage")
+    public String homePage(Model model, @RequestParam String email) {
+        int userId = userService.getUserId(email);
+        model.addAttribute("user", userService.getUser(userId));
         return "home/homepage";
     }
 
-
+    @PostMapping("/createWishlist")
+    public String createWishlist(Model model, @RequestParam String wishlistName,
+                                 @RequestParam String email) {
+      wishService.createWishlist(wishlistName, userService.getUserId(email)); //finder userId ud fra firstname, som ikke er unikt?
+        int userId = userService.getUserId(email);
+        model.addAttribute("user", userService.getUser(userId));
+        return "home/homepage";
+    }
 }
